6dc8d4e3298bf4262c983dd5d583fb5d0fc6ba14
Anna <filosoho@gmail.com>
2024-11-28 16:07:58 +0000

    modify app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import paginateRoutes from "./routes/paginateRoutes.js";
+import { handleErrors } from "./handlers/errorHandler.js";
 
 const app = express();
 
@@ -10,5 +11,12 @@ app.use(express.json());
 
 app.use("/functions/paginate", paginateRoutes);
 
+app.use(handleErrors);
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+export default app;
